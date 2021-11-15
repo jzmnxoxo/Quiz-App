b70b88914c3cbfc21d994d2bb552889865cc881d
felixhhw <u3557177@connect.hku.hk>
2021-11-15 17:02:45 +0800

    updated

--- Byngo/Category/CategoryDetail.swift
+++ Byngo/Category/CategoryDetail.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct CategoryDetail: View {
     
@@ -14,6 +15,7 @@ struct CategoryDetail: View {
     @State var category: Category
     
     @State var selectedDifficulty: Int = 1
+    @State var startedQuiz: Bool = false
     
     var categoryIndex: Int {
         categoryData.categories.firstIndex(where: {$0.id==category.id}) ?? 0
@@ -21,17 +23,81 @@ struct CategoryDetail: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0.0) {
-            Button("BEGINNER") {
+//            ZStack {
+//
+//                Rectangle()
+//                    .frame(width: 350, height: 70)
+//                    .foregroundColor(.gray)
+//                    .cornerRadius(10)
+//                Button("BEGINNER") {
+//                    selectedDifficulty = 1
+//                    startedQuiz = true
+//                }
+//            }
+            //beginner
+            Button( action: {
                 selectedDifficulty = 1
+                startedQuiz = true
+            }) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 350, height: 70)
+                        .foregroundColor(.gray)
+                        .opacity(0.1)
+                        .cornerRadius(10)
+                    Text("BEGINNER")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    
+                }
             }
             
-            Button("INTERMEDIATE") {
+            
+            //intermediate
+            Button( action: {
                 selectedDifficulty = 2
+                startedQuiz = true
+            }) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 350, height: 70)
+                        .foregroundColor(.gray)
+                        .opacity(0.1)
+                        .cornerRadius(10)
+                    Text("INTERMEDIATE")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    
+                }
             }
             
-            Button("ADVANCED") {
+            
+            //advanced
+            Button( action: {
                 selectedDifficulty = 3
+                startedQuiz = true
+            }) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 350, height: 70)
+                        .foregroundColor(.gray)
+                        .opacity(0.1)
+                        .cornerRadius(10)
+                    Text("ADVANCED")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    
+                }
             }
+            
+            
+            
             
             
 //            TextField("Title", text: $categoryData.categories[categoryIndex].title)
