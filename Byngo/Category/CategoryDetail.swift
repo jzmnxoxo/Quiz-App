b69868cf8ba90237c7d3ae6dd7ee041f0253f656
felixhhw <u3557177@connect.hku.hk>
2021-11-15 19:04:54 +0800

    Confirm Quiz View Added

--- Byngo/Category/CategoryDetail.swift
+++ Byngo/Category/CategoryDetail.swift
@@ -14,7 +14,11 @@ struct CategoryDetail: View {
     @Environment(\.presentationMode) var presentationMode
     @State var category: Category
     
-    @State var selectedDifficulty: Int
+    @State var selected: Int = 0
+    
+    @State private var isSelected = false
+    
+    let Difficulty = ["Beginner", "Intermediate", "Advanced"]
     
     var categoryIndex: Int {
         categoryData.categories.firstIndex(where: {$0.id==category.id}) ?? 0
@@ -34,8 +38,10 @@ struct CategoryDetail: View {
             
             //beginner
             Button( action: {
-                selectedDifficulty = 1
-                ConfirmQuizView(category: categoryData.categories[categoryIndex])
+                selected = 0
+                isSelected.toggle()
+                
+//                ConfirmQuizView(category: categoryData.categories[categoryIndex])
             }) {
                 ZStack {
                     Rectangle()
@@ -51,6 +57,63 @@ struct CategoryDetail: View {
                     
                 }
             }
+            .sheet(isPresented: $isSelected, onDismiss: didDismiss) {
+                VStack {
+                    HStack {
+                        Button( action: {
+                            isSelected.toggle()
+                        }) {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 100, height: 50)
+                                    .foregroundColor(.clear)
+                                    .opacity(0.1)
+                                    .cornerRadius(10)
+                                Text("Back")
+                                    .foregroundColor(.blue)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    Text(category.title)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(60)
+                    
+                    Text("Difficulty: \(Difficulty[selected])")
+                        .font(.title)
+                        .padding(55)
+                    
+                    Text("10 Qs")
+                        .font(.title)
+                        .padding(50)
+                    
+                    Spacer()
+                    Button( action: {
+                        //start quiz
+                    }) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 350, height: 70)
+                                .foregroundColor(.gray)
+                                .opacity(0.1)
+                                .cornerRadius(10)
+                            Text("START!")
+                                .font(.title)
+                                .foregroundColor(.black)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                    }
+                }
+            }
+            
             
             Rectangle()
                 .frame(width: 350, height: 30)
@@ -58,8 +121,8 @@ struct CategoryDetail: View {
             
             //intermediate
             Button( action: {
-                selectedDifficulty = 2
-                ConfirmQuizView(category: categoryData.categories[categoryIndex])
+                selected = 1
+                isSelected.toggle()
             }) {
                 ZStack {
                     Rectangle()
@@ -75,6 +138,61 @@ struct CategoryDetail: View {
                     
                 }
             }
+            .sheet(isPresented: $isSelected, onDismiss: didDismiss) {
+                VStack {
+                    HStack {
+                        Button( action: {
+                            isSelected.toggle()
+                        }) {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 100, height: 50)
+                                    .foregroundColor(.clear)
+                                    .opacity(0.1)
+                                    .cornerRadius(10)
+                                Text("Back")
+                                    .foregroundColor(.blue)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    Text(category.title)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(60)
+                    
+                    Text("Difficulty: \(Difficulty[selected])")
+                        .font(.title)
+                        .padding(55)
+                    
+                    Text("10 Qs")
+                        .font(.title)
+                        .padding(50)
+                    Spacer()
+                    Button( action: {
+                        //start quiz
+                    }) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 350, height: 70)
+                                .foregroundColor(.gray)
+                                .opacity(0.1)
+                                .cornerRadius(10)
+                            Text("START!")
+                                .font(.title)
+                                .foregroundColor(.black)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                    }
+                }
+            }
             
             Rectangle()
                 .frame(width: 350, height: 30)
@@ -82,8 +200,8 @@ struct CategoryDetail: View {
             
             //advanced
             Button( action: {
-                selectedDifficulty = 3
-                ConfirmQuizView(category: categoryData.categories[categoryIndex])
+                selected = 2
+                isSelected.toggle()
             }) {
                 ZStack {
                     Rectangle()
@@ -99,6 +217,61 @@ struct CategoryDetail: View {
                     
                 }
             }
+            .sheet(isPresented: $isSelected, onDismiss: didDismiss) {
+                VStack {
+                    HStack {
+                        Button( action: {
+                            isSelected.toggle()
+                        }) {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 100, height: 50)
+                                    .foregroundColor(.clear)
+                                    .opacity(0.1)
+                                    .cornerRadius(10)
+                                Text("Back")
+                                    .foregroundColor(.blue)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    Text(category.title)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(60)
+                    
+                    Text("Difficulty: \(Difficulty[selected])")
+                        .font(.title)
+                        .padding(55)
+                    
+                    Text("10 Qs")
+                        .font(.title)
+                        .padding(50)
+                    Spacer()
+                    Button( action: {
+                        //start quiz
+                    }) {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 350, height: 70)
+                                .foregroundColor(.gray)
+                                .opacity(0.1)
+                                .cornerRadius(10)
+                            Text("START!")
+                                .font(.title)
+                                .foregroundColor(.black)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                            
+                        }
+                    }
+                }
+            }
             
             Spacer()
             
@@ -108,6 +281,10 @@ struct CategoryDetail: View {
         }
         .padding()
         
+    }
+    
+    func didDismiss() {
+        // Handle the dismissing action.
     }
 }
 
@@ -119,3 +296,32 @@ struct CategoryDetail_Previews: PreviewProvider {
             .environmentObject(ModelDataCat())
     }
 }
+
+
+
+//struct ShowDifficulty: View {
+//    @State private var isShowingSheet = false
+//    var body: some View {
+//        Button(action: {
+//            isShowingSheet.toggle()
+//        }) {
+//            Text("Show License Agreement")
+//        }
+//        .sheet(isPresented: $isShowingSheet,
+//               onDismiss: didDismiss) {
+//            VStack {
+//                Text("License Agreement")
+//                    .font(.title)
+//                    .padding(50)
+//                Text("""
+//                        Terms and conditions go here.
+//                    """)
+//                    .padding(50)
+//                Button("Dismiss",
+//                       action: { isShowingSheet.toggle() })
+//            }
+//        }
+//    }
+//
+
+
