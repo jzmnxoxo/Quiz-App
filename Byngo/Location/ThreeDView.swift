2a8c182ee614d1f95ee42803a8973bcee29da07e
jzmnxoxo <jasminelee502@gmail.com>
2021-11-18 01:48:05 +0800

    minor update

--- Byngo/Location/ThreeDView.swift
+++ Byngo/Location/ThreeDView.swift
@@ -13,7 +13,7 @@ struct ThreeDView:View{
     @ObservedObject var choice = Choice()
     
     @State var models = [
-        Model(id: 0, name:"Star Ferry", modelName: "ferry.usdz",details:"Star Ferry boats have been transporting passengers from Hong Kong Island to Kowloon and back for over a century and offer a unique vantage point on the city’s beautiful harbour."),
+        Model(id: 0, name:"Star Ferry", modelName: "ship.usdz",details:"Star Ferry boats have been transporting passengers from Hong Kong Island to Kowloon and back for over a century and offer a unique vantage point on the city’s beautiful harbour."),
         Model(id: 1, name: "Hong Kong International Airport", modelName: "plane.usdz", details: "Designed by acclaimed British architect Lord Norman Foster, this awesome state-of-the-art structure completed in 1998 was voted one of the Top 10 Construction Achievements of the 20th Century by the Construction Industry Manufacturers Association. Located at Chek Lap Kok on Lantau Island, Hong Kong International Airport is linked to the city’s urban areas via an impressive chain of highways, railways and bridges. "),
         Model(id: 2, name: "Hong Kong General Post Office", modelName: "post.usdz", details: "The General Post Office is the headquarters of Hongkong Post. Built in 1976, it is located at Connaught Place, Central, Hong Kong. The office was adjacent to the former Star Ferry Pier, and it is adjacent to Jardine House and the International Finance Centre")]
     
